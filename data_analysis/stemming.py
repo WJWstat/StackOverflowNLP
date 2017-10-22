22d93fe3eedb4b00f1099a1fe5a36f45b6bfe399
Suyash Lakhotia <suyashlakhotia@gmail.com>
2017-10-22 17:25:17 +0800

    Updated Stemming to Use nltk.word_tokenize()

--- data_analysis/stemming.py
+++ data_analysis/stemming.py
@@ -1,6 +1,6 @@
 import re
 import pickle
-from nltk.stem.porter import PorterStemmer
+import nltk
 
 
 def stem_posts():
@@ -10,37 +10,37 @@ def stem_posts():
     with open('data_analysis/stop_words.txt', 'r') as f:
         stop_words = [line.rstrip() for line in f]
 
-    porter_stemmer = PorterStemmer()
+    porter_stemmer = nltk.stem.porter.PorterStemmer()
     stems = {}  # dictionary of stems {stem: { 'orig_words': (set), 'count': 123 }, ...}
 
     for post in posts:
         post = re.sub(r'<code>.*</code>', '', post)  # remove inline code snippets
 
         for paragraph in post.split('\n'):
-            paragraph = paragraph.lower()  # lowercase
-            paragraph = re.sub(r'[^a-z]', ' ', paragraph)  # replace non-alphabet characters with ' '
-            paragraph = paragraph.strip()
+            paragraph = paragraph.lower()  # lowercase text
+            tokens = nltk.word_tokenize(paragraph)
 
-            for word in paragraph.split(' '):
-                if word in stop_words or word == '':
+            for token in tokens:
+                token = re.sub(r'[^a-z]', '', token)  # remove any non-alphabet characters
+                if token == '' or token in stop_words:
                     continue
 
-                stem = porter_stemmer.stem(word)
+                stem = porter_stemmer.stem(token)
 
                 if stem in stems:
                     stems[stem]['count'] += 1
-                    stems[stem]['orig_words'].add(word)
+                    stems[stem]['orig_words'].add(token)
                 else:
                     stems[stem] = {}
                     stems[stem]['count'] = 1
-                    stems[stem]['orig_words'] = set([word])
+                    stems[stem]['orig_words'] = set([token])
 
-    # counter = 50
-    # for s in sorted(stems.items(), key=lambda k_v: k_v[1]['count'], reverse=True):
-    #     print(s)
-    #     counter -= 1
-    #     if counter == 0:
-    #         break
+    counter = 50
+    for s in sorted(stems.items(), key=lambda k_v: k_v[1]['count'], reverse=True):
+        print(s)
+        counter -= 1
+        if counter == 0:
+            break
 
     with open('pickles/stems.pkl', 'wb') as f:
         pickle.dump(stems, f)
