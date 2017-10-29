77cdcc952d28cbb116437457086afa0b30922df7
Suyash Lakhotia <suyashlakhotia@gmail.com>
2017-10-29 18:32:05 +0800

    Added Code for Post Length Plot

---
+++ dataset_collection/post_stats.py
@@ -0,0 +1,25 @@
+import pickle
+import re
+import matplotlib.pyplot as plt
+import nltk
+
+with open('../pickles/posts.pkl', 'rb') as f:
+    posts = pickle.load(f)
+
+post_length = {}
+for post in posts:
+    post = re.sub(r'<code>.*</code>', '', post)  # remove inline code snippets
+    words = nltk.word_tokenize(post)
+
+    if len(words) in post_length:
+        post_length[len(words)] += 1
+    else:
+        post_length[len(words)] = 1
+
+plt.figure()
+plt.bar(list(post_length.keys()), list(post_length.values()), width=1.0)
+plt.xlabel('Word Count')
+plt.ylabel('No. of Posts')
+plt.tight_layout()
+plt.savefig('../plots/post_length.png', dpi=800)
+plt.close('all')
