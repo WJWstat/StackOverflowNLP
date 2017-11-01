83c87301694dcc91169d5284893370a8026a4384
Nikhil Venkatesh <nikv96@gmail.com>
2017-11-01 12:30:00 +0800

    Updated Code For Running from Root

--- dataset_collection/post_stats.py
+++ dataset_collection/post_stats.py
@@ -3,7 +3,7 @@ import re
 import matplotlib.pyplot as plt
 import nltk
 
-with open('../pickles/posts.pkl', 'rb') as f:
+with open('pickles/posts.pkl', 'rb') as f:
     posts = pickle.load(f)
 
 post_length = {}
@@ -21,5 +21,5 @@ plt.bar(list(post_length.keys()), list(post_length.values()), width=1.0)
 plt.xlabel('Word Count')
 plt.ylabel('No. of Posts')
 plt.tight_layout()
-plt.savefig('../plots/post_length.png', dpi=800)
+plt.savefig('plots/post_length.png', dpi=800)
 plt.close('all')
